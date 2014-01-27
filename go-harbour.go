0960ffa348a1325e9a4719f6a96049dc9955bb7d
Ernad Husremovic <hernad@bring.out.ba>
2014-01-27 12:52:57 +0100

    hbct

--- go-harbour.go
+++ go-harbour.go
@@ -2,7 +2,7 @@ package harbour
 
 /*
   #cgo CFLAGS: -I/usr/local/include/harbour -I/usr/include/postgresql
-  #cgo LDFLAGS: -L/usr/local/lib/harbour -lpq -lhbpgsql  -lharbour
+  #cgo LDFLAGS: -L/usr/local/lib/harbour -lpq -lhbct -lhbpgsql  -lharbour
   #include <hbvmint.h>
   #include <hbapi.h>
   #include <hbstack.h>
@@ -22,6 +22,10 @@ import "C"
 
 import(
   "fmt"
+  "archive/zip"
+  "log"
+  "os"
+  "io"
 //  "unsafe"
 )
 
@@ -37,6 +41,7 @@ func HrbLoad(file string, mode int) {
    //C.hb_vmSetDefaultGT(C.CString("trm"));
    C.hb_vmInit( C.HB_BOOL(0) )
 
+/*
    LoadDo("hrbext.hrb")     
  
    pDynSym := C.hb_dynsymGet(C.CString("B"))
@@ -54,22 +59,69 @@ func HrbLoad(file string, mode int) {
    fmt.Println("harbour memvar B (nakon inputa) -> go string")
    pItem = C.hb_dynsymGetMemvar(pDynSym)
    fmt.Printf("Uzimam iz harbour memvar B(2): %s\n", C.GoString(C.hb_itemGetC(pItem))) 
+*/
 
-   LoadDo("pg_test.hrb") 
-   fmt.Println("ucitavam tpostgre harbour klasu")   
-   LoadDo("tpostgre.hrb")
+   //LoadDo("pg_test.hrb") 
+
+   //fmt.Println("ucitavam tpostgre harbour klasu")   
+   //LoadDo("tpostgre.hrb")
 
    //fmt.Println("test pgtest")
    //LoadDo("hbgo.hrb") 
    
+
+//   LoadZip("common")
+//   LoadHrb("common/" + "f18_ini_utils.hrb")
+//   LoadHrb("common/" + "f18_init_login.hrb")
+//   LoadHrb("common/" + "f18_init_semaphores.hrb")
+     LoadDoHrb("common.hrb")
+
+
+   LoadZip("fin")
+
    //C.hb_conOutStd( C.CString("Ernad Husremovic\n"), C.HB_SIZE(20) ) 
    fmt.Println("kraj")
+
+
 
 }
 
 
-func LoadDo(file string) {
+func LoadZip(file string) {
 
+r, err := zip.OpenReader(file + ".zip")
+if err != nil {
+     log.Fatal(err)
+}
+defer r.Close()
+
+// Iterate through the files in the archive,
+// printing some of their contents.
+for _, f := range r.File {
+            fmt.Printf("Contents of %s:\n", f.Name)
+            rc, err := f.Open()
+            if err != nil {
+                    log.Fatal(err)
+            }
+
+
+            d, errd := os.Create(file + "/" + f.Name)
+	    if errd != nil {
+		log.Fatal(errd)
+	    }
+	    if _, errc := io.Copy(d, rc); errc != nil {
+		d.Close()
+		log.Fatal(errc)
+	    }
+            rc.Close()
+            fmt.Println()
+}
+
+}
+
+func LoadDoHrb(file string) {
+
+  fmt.Println("load and do hrb ", file)
   pBody := C.hb_hrbLoadFromFile( C.CString(file), C.HB_USHORT(1))
   C.hb_hrbDo( pBody, 0, nil );
  
@@ -79,5 +131,4 @@ func LoadHbPostgresqlC() {
         var conn *C.PGconn
         C.PQreset(conn)
 }
-
 
--- linkaj_me.prg
+++ linkaj_me.prg
@@ -48,3 +48,6 @@
    Lo_Import()
    Lo_Export()
    Lo_Unlink()
+
+   NumToken()
+
